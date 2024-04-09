5114e1e63eab9d2991e0c36035fedb2ee455355b
Murad Muradli <mmuradmuradlii@gmail.com>
2024-04-09 23:54:18 +0200

    Create basic modal structure and configure Zustand

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function SetupPage() {
-	return <div className="p-4">
-		<UserButton afterSignOutUrl="/" />
-	</div>;
+	const onOpen = useStoreModal((state) => state.onOpen);
+	const isOpen = useStoreModal((state) => state.isOpen);
+
+	useEffect(() => {
+		if (!isOpen) {
+			onOpen();
+		}
+	}, [isOpen, onOpen]);
+
+	return <div className="p-4"></div>;
 }
